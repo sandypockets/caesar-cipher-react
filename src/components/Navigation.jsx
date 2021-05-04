7f31e47b6aa0bc889b00311ed277398fae1447e8
sandypockets <zac@znacreative.com>
2021-05-03 23:34:16 -0400

    Adjust nav text colour

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Navigation.css';
+import './Navigation.scss';
 
 export const Navigation = () => {
   return(
     <nav className="nav">
-      <span id="logo-type">App Name</span>
+      <span id="logo-type">Caesar Cipher</span>
       <span className="nav-span-text">Some Text</span>
     </nav>
   )
