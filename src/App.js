a75b869b2897980bdb59d4e584d0a6a996cff95f
sandypockets <zac@znacreative.com>
2021-05-04 00:54:47 -0400

    Move state to component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Navigation } from "./components/Navigation.jsx";
 import { Form } from "./components/Form";
 
 function App() {
+
   return (
     <div className="App">
       <Navigation />
