bb5035a98bb739853ee326d5e18631e671aafc68
sandypockets <zac@znacreative.com>
2021-05-04 23:30:37 -0400

    Add num written as word validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {runCipher} from "../helpers/cipher";
 import {CipherText} from "./CipherText";
+import {checkIfNum} from "../helpers/validateShiftKey";
 import "./Form.scss";
 
 export const Form = () => {
@@ -18,7 +19,7 @@ export const Form = () => {
   const getCipherKey = event => {
     const { value } = event.target;
     setUserInput(value)
-    setCipherKey(value)
+    setCipherKey(checkIfNum(value))
     setTimeout(() => {
       setForm(true)
     }, 2000)
